5dd872e4bdd82521a1dc466fbd7aab3dbd3719c1
Olaiwola Bolaji <olaiwola.bolaji@gmail.com>
2019-07-30 11:06:18 +0100

    Getting my feet wet with Javascript again

---
+++ Javascript_One/controlflow.js
@@ -0,0 +1,19 @@
+
+
+var x = 1;
+
+
+while(x < 10){
+        console.log("Checking through the series of numbers from 1 to 10");
+
+        x = x + 1;
+        if(x % 2 === 0){
+                console.log(x + " is divisible by 2");
+                
+        }else{
+                console.log(x + " is not divisible by 2");
+                
+        }
+
+        
+}
