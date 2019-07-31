2bc2846540fd7d56b3742c2dc9000ac038fae47d
Olaiwola Bolaji <olaiwola.bolaji@gmail.com>
2019-07-31 10:41:11 +0100

    Finished the first Javascript part of the course
    Did my best to use stackoverflow and MDN to resolve this question

---
+++ Javascript_One/forloop.js
@@ -0,0 +1,16 @@
+// This is to try out the for loop
+
+for (var i = 0; i < 5; i++) {
+        console.log("Number is " + i);
+        
+} 
+
+
+var word = "ABCDEFGHIJK";
+
+for (var i = 0; i < word.length; i++){
+        console.log(word[i]);
+        
+}
+
+
---
+++ Javascript_One/javascriptlevel_one.js
@@ -0,0 +1,15 @@
+var firstName = prompt('What is your first name?');
+var lastName = prompt('What is your last name?');
+var age = prompt("how old are you?");
+var height = prompt("How tall are you in centimetres?");
+var petName = prompt("What is the name of yout pet?");
+alert("Thank you so much for the information")
+
+
+if ((firstName[0] === lastName[0]) && (age > 20 && age < 30) && (height >= 170) && (petName[-1] = 'y')) {
+        console.log("Visit https://www.livingwordmedia.org to download the latest audio materials for your spiritual growth");
+
+} else {
+        console.log("There is nothing to see here. Arigato!");
+        
+}
